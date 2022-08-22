0ae4dfeb6ea00422cd1278131376c4c1fcedb5d6
Mrrebrik <itibsoft@gmail.com>
2022-08-22 18:19:14 +0700

    - switching menu to game from canvas group; create simple tutorial click to UI

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,4 +1,6 @@
+using Assets.Scripts.Audio;
 using Assets.Scripts.Utils;
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +9,9 @@ namespace Assets.Scripts.Game
 {
 	public class GameManager : SingletonMono<GameManager>
 	{
+		[SerializeField] private CanvasGroup _gameHud;
+		[SerializeField] private CanvasGroup _menuUI;
+
 		public override void Awake()
 		{
 			base.Awake();
@@ -14,6 +19,17 @@ namespace Assets.Scripts.Game
 
 		public void StartGame()
 		{
+			_gameHud.alpha = 0f;
+			_gameHud.gameObject.SetActive(true);
+
+			_gameHud.DOFade(1f, 0.5f);
+			_menuUI.DOFade(0f, 0.5f).onComplete += () =>
+			{
+				_menuUI.gameObject.SetActive(false);
+			};
+
+			AudioManager.Instance.PlayMusic(TypeAudio.MusicGame);
+
 
 		}
 	}
